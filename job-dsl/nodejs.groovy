4eb0276f8eb9a4dbeb76e9cca17fb62d5725d9fe
pyrek7 <fiderek@gmail.com>
2021-04-25 13:20:51 +0200

    add dsl job file

---
+++ job-dsl/nodejs.groovy
@@ -0,0 +1,21 @@
+job ('job example for npm')
+{
+    scm {
+            git('https://github.com/pyrek7/nodejs.git') { node ->
+                node / gitConfigName('DSL script')
+                node / gitConfigEmail('jenkins-dsl-script@altkom.com')
+            }
+    }
+
+    triggers {
+        scm('H/5 * * * *')
+    }
+
+    mappers {
+        nodejs('nodejs')
+    }
+
+    steps {
+        shell("npm install")
+    }
+}
